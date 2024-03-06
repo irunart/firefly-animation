5780b4f59dace3ccfa12746b74d760686eac847e
Pili Hu <e@hupili.net>
2024-03-07 00:17:20 +0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -15,4 +15,8 @@ cp "${CWD}/dist/static/"* "${STATIC_PATH}"
 
 FIREFLY_PATH="${FIREFLY_PATH:-${TARGET_PATH}/firefly_animation}"
 [[ -d "${FIREFLY_PATH}" ]] || mkdir -p "${FIREFLY_PATH}"
-cp "${CWD}/dist/index.html" "${FIREFLY_PATH}"
+cp -f "${CWD}/dist/index.html" "${FIREFLY_PATH}"
+
+FIREFLY_STATIC_PATH="${FIREFLY_STATIC_PATH:-${TARGET_PATH}/static}"
+[[ -d "${FIREFLY_STATIC_PATH}" ]] || mkdir -p "${FIREFLY_STATIC_PATH}"
+cp -f "${CWD}/dist/static/"* "${FIREFLY_STATIC_PATH}"
